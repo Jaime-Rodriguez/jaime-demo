7605e063875e655b463537a5555ce678e9cb5366
Jaime Rodriguez <rodriguez.jaime.a@gmail.com>
2026-09-21 15:26:28 -0700

    add navbar and page structure

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex gap-6 p-4 border-b border-gray-200">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/resume">Resume</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
+  );
+}
